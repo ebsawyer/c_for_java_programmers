902dcf03b4f07bbd09f4a30c7321a0d6c7d56303
Ethan Sawyer <ethan.b.sawyer@gmail.com>
2020-07-06 14:25:20 -0500

    Completed code for the objects challenge.
    Signed-off-by: Ethan Sawyer <ethan.b.sawyer@gmail.com>

--- objects.c
+++ objects.c
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Employee {
     char *name;
@@ -14,11 +15,38 @@ struct Employee *new_Employee(char *name, int id) {
     if (this == NULL) {
         return NULL; // Out of memory...
     }
-    this->name = name;
+    this->name = strdup(name);
     this->id = id;
     return this;
 }
 
+char *strdup(const char *s) {
+    char *t = (char *) malloc(strlen(s) + 1);
+    strcpy(t, s);
+    return t;
+}
+
+char *Employee_get_name(struct Employee *this) {
+    return this->name;
+}
+
+int Employee_get_id(struct Employee *this) {
+    return this->id;
+}
+
+void Employee_print(struct Employee *this) {
+    printf("Employee[%s, %d]", this->name, this->id);
+}
+
+void Employee_free(struct Employee *this) {
+    free(this->name);
+    free(this);
+}
+
 int main(int argc, char *argv[]) {
     struct Employee *e = new_Employee("Alan Turing", 123);
+    printf("%s\n", Employee_get_name(e));
+    printf("%d\n", Employee_get_id(e));
+    Employee_print(e);
+    Employee_free(e);
 }
