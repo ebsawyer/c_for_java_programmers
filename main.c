300cb20765a37499dd9c65c7ee7d03a890d55b56
Ethan Sawyer <ethan.b.sawyer@gmail.com>
2020-07-05 22:33:34 -0500

    Second commit.
    Signed-off-by: Ethan Sawyer <ethan.b.sawyer@gmail.com>

--- main.c
+++ main.c
@@ -5,9 +5,4 @@
 
 int main(int argc, char *argv[]) {
     printf("Hello world!\n");
-
-    char **s_array = (char **) calloc(5, sizeof(char *));
-    for (int i = 0; i < 5; i++) {
-        s_array[i] = (char *) calloc(5, sizeof(char));
-    }
 }
